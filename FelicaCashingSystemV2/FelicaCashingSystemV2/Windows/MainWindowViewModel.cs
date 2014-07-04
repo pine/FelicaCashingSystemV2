a3ee9aad2a499833e0c4f52e24fa570a2ddf0263
MIZUNE Pine <pinemz@gmail.com>
2014-07-04 23:25:45 +0900

    WIP

--- FelicaCashingSystemV2/FelicaCashingSystemV2/Windows/MainWindowViewModel.cs
+++ FelicaCashingSystemV2/FelicaCashingSystemV2/Windows/MainWindowViewModel.cs
@@ -14,57 +14,5 @@ namespace FelicaCashingSystemV2.Windows
 {
     class MainWindowViewModel : MoneyViewModel
     {
-        public MainWindowViewModel()
-        {
-            this.User = App.Current.User;
-            App.Current.UserChanged += App_UserChanged;
-        }
-
-        private void App_UserChanged(object sender, FelicaData.User e)
-        {
-            this.User = e;
-        }
-
-        private FelicaData.NullableUser user = new FelicaData.NullableUser();
-        private FelicaData.User User
-        {
-            get { return this.user.User; }
-            set
-            {
-                this.user.User = value;
-                this.OnPropertyChanged("Name");
-                this.OnPropertyChanged("Money");
-                this.OnPropertyChanged("AvatarSource");
-            }
-        }
-
-        public string Name
-        {
-            get
-            {
-                return this.user.Name;
-            }
-        }
-
-        public int Money
-        {
-            get
-            {
-                return this.user.Money;
-            }
-        }
-
-        public ImageSource AvatarSource
-        {
-            get
-            {
-                if (this.user.Avatar != null)
-                {
-                    return this.user.Avatar.ToBitmap().ToBitmapSource();
-                }
-
-                return null;
-            }
-        }
     }
 }
