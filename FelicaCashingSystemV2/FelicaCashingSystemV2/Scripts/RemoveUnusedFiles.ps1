9888ad28d1e4635ea2d3856ec8146aa81927bead
Pine Mizune <pinemz@gmail.com>
2015-01-26 10:41:21 +0900

    ビルド完了後に不要ファイルを削除するように変更#

---
+++ FelicaCashingSystemV2/FelicaCashingSystemV2/Scripts/RemoveUnusedFiles.ps1
@@ -0,0 +1,7 @@
+﻿Write "Start removing unused files"
+
+$files = @("*.pdb","*.xml","*vshost*")
+$folders = @("de","es","fr","it","ko","ru","zh-*")
+
+$files | % { Remove-Item -Force $_ }
+$folders | % { Remove-Item -Force -Recurse $_ }
