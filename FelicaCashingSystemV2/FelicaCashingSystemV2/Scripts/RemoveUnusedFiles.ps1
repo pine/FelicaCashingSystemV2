8c3939ea81f6cdc985c128a23d77879a413d29fa
Pine Mizune <pinemz@gmail.com>
2015-01-29 06:01:49 +0900

    Fix a script

--- FelicaCashingSystemV2/FelicaCashingSystemV2/Scripts/RemoveUnusedFiles.ps1
+++ FelicaCashingSystemV2/FelicaCashingSystemV2/Scripts/RemoveUnusedFiles.ps1
@@ -3,5 +3,14 @@
 $files = @("*.pdb","*.xml","*vshost*")
 $folders = @("de","es","fr","it","ko","ru","zh-*")
 
-$files | % { Remove-Item -Force $_ }
-$folders | % { Remove-Item -Force -Recurse $_ }
+$files | % {
+  if (Test-Path $_) {
+    Remove-Item -Force $_
+  }
+}
+
+$folders | % {
+  if (Test-Path $_) {
+    Remove-Item -Force -Recurse $_
+  }
+}
